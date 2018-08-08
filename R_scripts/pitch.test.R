f465d093fe353ec18186b012f78d08bde438956c
Brandon N <37309728+bcnotario@users.noreply.github.com>
2018-08-07 23:46:17 -0700

    Update pitch.test.R

--- R_scripts/pitch.test.R
+++ R_scripts/pitch.test.R
@@ -10,7 +10,7 @@ pit.test <- function(pit.dat,sample.n){
   test.all <- matrix(0,sample.n,9)
   colnames(test.all) <- c('i','ID','Pitches','Type0 Accuracy','Type1 Accuracy','Type2 Accuracy','Dev0','Dev1','Dev2')
   for(j in test.s){
-  test.all[which(test.s==j),] <- data.matrix(pit.mod(pit.dat$pit.all,j))
+  test.all[which(test.s==j),] <- data.matrix(pit.mod(pitch.dat$pit.all,j))
   }
   return(test.all)
 }
